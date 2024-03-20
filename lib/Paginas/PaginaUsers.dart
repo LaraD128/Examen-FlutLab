6d526c06e8fd27eafd666ae49c0aa2c998efa568
DIEGO_SEBASTIAN_LARA_BAEZA <a.21308051280926@cbtis128.edu.mx>
2024-03-20 15:14:06 +0000

    Actualizado

---
+++ lib/Paginas/PaginaUsers.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PaginaUsers extends StatelessWidget {
+  const PaginaUsers({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Usuarios",
+        style: TextStyle(fontSize: 30),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:lara0926/Paginas/PaginaHome.dart';
+import 'package:lara0926/Paginas/PaginaUsers.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyAppNavigatorBar0926());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MyAppNavigatorBar0926 extends StatefulWidget {
+  @override
+  State<MyAppNavigatorBar0926> createState() => _MyAppNavigatorBar0926State();
+}
+
+class _MyAppNavigatorBar0926State extends State<MyAppNavigatorBar0926> {
+  int _paginaActual = 1;
+
+  List<Widget> _paginas = [PaginaHome(), PaginaUsers()];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      title: "Material app0926",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Material AppBar0926"),
+        ),
+        body: _paginas[_paginaActual],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            setState(() {
+              _paginaActual = index;
+            });
+          },
+          currentIndex: _paginaActual,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Inicio"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.supervised_user_circle), label: "Usuarios")
+          ],
         ),
       ),
     );
